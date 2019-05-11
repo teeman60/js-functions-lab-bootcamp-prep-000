b0aef1a9afd23ef3a6a2dc170a7680da365283e1
Taiye Salami <taiye41@gmail.com>
2019-05-11 03:40:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(Justin) {
-  return "Happy holidays, ${name}!"
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${Justin}!"
 }
